764509ed6ee0ba197a7005a14f39f30ce295ff2c
Zick Norizkue <plazzedd@outlook.com>
2025-07-05 04:28:38 +0300

    new pravki

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,27 +1,34 @@
 from fastapi import APIRouter, Depends, HTTPException
-from sqlmodel import Session
+from sqlmodel import Session, select
 from pydantic import BaseModel
 from app.models.user import User
 from app.database import engine
 
 router = APIRouter(prefix="/users")
 
-
 def get_session():
     with Session(engine) as session:
         yield session
 
-
-# Pydantic модель для безопасности
+# Модель для патча роли
 class UserRolePatch(BaseModel):
     is_driver: bool
 
+@router.get("/", response_model=list[User])
+def list_users(session: Session = Depends(get_session)):
+    users = session.exec(select(User)).all()
+    return users
 
 @router.patch("/{user_id}", response_model=User)
 def update_user_role(
-    user_id: int, payload: UserRolePatch, session: Session = Depends(get_session)
+    user_id: int,
+    payload: UserRolePatch,
+    session: Session = Depends(get_session),
 ):
+    users = session.exec(select(User)).all()
+    print("ВСЕ ЮЗЕРЫ:", users)
     user = session.get(User, user_id)
+    print("ИЩЕМ ЮЗЕРА:", user)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     user.is_driver = payload.is_driver
