445ddf68ad88a0f84cf8a717519f1a6fc533b5c7
Zick Norizkue <plazzedd@outlook.com>
2025-07-12 12:05:19 +0300

    исправил модель поездок

--- app/models/trip.py
+++ app/models/trip.py
@@ -1,15 +1,15 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
 from app.utils.time import get_utc_now
-from datetime import datetime
+from datetime import date, time
 
 
 class Trip(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     from_: str = Field(alias="from_")
     to: str
-    date: datetime.date
-    time: datetime.time
+    date: date
+    time: time
     seats: int
     price: int
     owner_id: int = Field(foreign_key="user.id")
