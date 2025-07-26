894d8e993bac4b2d37dcbd8fc98627db928cff26
Zick Norizkue <plazzedd@outlook.com>
2025-07-27 01:29:58 +0300

    new tarrifsss

--- app/models/booking.py
+++ app/models/booking.py
@@ -1,11 +1,6 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
 from app.utils.time import get_utc_now
-from datetime import datetime, timezone
-
-
-def utcnow():
-    return datetime.now(timezone.utc)
 
 
 class Booking(SQLModel, table=True):
@@ -14,4 +9,3 @@ class Booking(SQLModel, table=True):
     user_id: int
     status: str = Field(default="pending")
     created_at: Optional[str] = Field(default_factory=get_utc_now)
-    created_at_for_timer: datetime = Field(default_factory=utcnow)
